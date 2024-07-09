e42b0d1dbcb0d90f96a2385565ef1a9c1f6fc9f7
Ho Kim <ho.kim@ulagbulag.io>
2024-07-09 20:32:09 +0000

    feat: update default home page title

--- crates/cassette/src/pages/home.rs
+++ crates/cassette/src/pages/home.rs
@@ -5,7 +5,7 @@ pub fn app() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "hello world" }</h1>
+            <h1>{ "Cassette" }</h1>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
         </main>
     }
