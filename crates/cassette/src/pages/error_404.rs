a965f7b4d2c542a69f73e284b7d29fcc9a7c6023
Ho Kim <ho.kim@ulagbulag.io>
2024-07-10 11:57:54 +0000

    fix: rendering error on 404 page

--- crates/cassette/src/pages/error_404.rs
+++ crates/cassette/src/pages/error_404.rs
@@ -3,9 +3,11 @@ use yew::prelude::*;
 #[function_component(Error404)]
 pub fn error_404() -> Html {
     html! {
-        <main>
-            <h1>{ "404" }</h1>
-            <span class="subtitle">{ "Not Found :/" }</span>
+        <main class="error">
+            <div>
+                <h1 class="error">{ "404" }</h1>
+                <span class="subtitle">{ "Not Found :/" }</span>
+            </div>
         </main>
     }
 }
